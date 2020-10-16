ff4dfe81637b01a84e7c5f3f0000593b079e00fe
assouaalbert <assouaalbert@gmail.com>
2020-10-16 06:57:43 +0300

    Created the mnavigation main navigation menu

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+<link rel="stylesheet" href="css/style.css">
+
+<body>
+    <!-- /* ----------------------------- Navigation Bar ----------------------------- */ -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">Berto</a>
+            <div class="navbar-toggle" data-toggle="collapse" data-target="#collapseNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </div>
+            <div class="collapse navbar-collapse" id="collapseNavbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a href="myprofile.php" class="nav-link"> <i class="fa fa-user"
+                                aria-hidden="true"></i> My Profile</a></li>
+                    <li class="nav-item"><a href="dashboard.php" class="nav-link"> <i
+                                class="fas fa-notes-medical    "></i>
+                            Dashboard</a></li>
+                    <li class="nav-item"><a href="post.php" class="nav-link">Post</a></li>
+                    <li class="nav-item"><a href="categories.php" class="nav-link">Categories</a></li>
+                    <li class="nav-item"><a href="admin.php" class="nav-link">Manage Admins</a></li>
+                    <li class="nav-item"><a href="comments.php" class="nav-link">Comments</a></li>
+                    <li class="nav-item"><a href="log.php?page=1" class="nav-link">Live Blog</a></li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="logout.php" class="nav-link"> <i class="fas fa-sign-out-alt    "></i> Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+    integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+    integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+</script>
+
+</html>
